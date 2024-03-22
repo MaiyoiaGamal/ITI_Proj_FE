35ded18fe42146972cac83b0a591045bab3ee331
Anas01 <anassobhy07@gmail.com>
2024-03-22 19:07:37 +0200

    delete-attendace

--- src/app/Component/employee-attendance/employee-attendance.component.ts
+++ src/app/Component/employee-attendance/employee-attendance.component.ts
@@ -96,7 +96,7 @@ export class EmployeeAttendanceComponent implements OnInit {
       })
     }
    
-    deleteAttendance(EmployeeId: number, item: any): void {
+    deleteAttendance(EmployeeId: number, item: any ,date:string): void {
       Swal.fire({
         title: 'Are you sure?',
         text: 'You want to delete this item?',
@@ -108,10 +108,10 @@ export class EmployeeAttendanceComponent implements OnInit {
         cancelButtonText: 'Cancel'
       }).then((result) => {
         if (result.isConfirmed) {
-          this.service.deleteEmployeeAttendance(EmployeeId)
+          this.service.deleteEmployeeAttendance(EmployeeId,date)
             .subscribe(() => {
               Swal.fire('Success', 'Attendance record deleted successfully', 'success');
-              // Optionally, you can remove the deleted item from the array
+              
               const index = this.allemp.indexOf(item);
               if (index !== -1) {
                 this.allemp.splice(index, 1);
